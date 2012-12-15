aa849b671bb493aa40e6c0edefc57b4ff34b40aa
John Parron <Holosynthetic@Johns-MacBook.local>
2012-12-15 10:29:01 -0500

    Initial Commit

---
+++ ZenLegacy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZenLegacy
+//
+//  Created by John Parron on 12/15/12.
+//  Copyright (c) 2012 John Parron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZenLegacy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZenLegacy
+//
+//  Created by John Parron on 12/15/12.
+//  Copyright (c) 2012 John Parron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
